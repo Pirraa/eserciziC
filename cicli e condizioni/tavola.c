732b7eb396c5b20896ad8f05084962a98282439c
Piraa <pirrafranci2004@gmail.com>
2023-10-19 21:07:11 +0200

    tavola pitagorica colorata

--- cicli e condizioni/tavola.c
+++ cicli e condizioni/tavola.c
@@ -2,15 +2,24 @@
 
 int main()
 {
+    int color=31;
     printf("Tavola pitagorica");
 
-    for(int i=1;i<10;i++)
+    for(int i=1;i<11;i++)
     {
+        if(color==37)
+            color=31;
+        if(color==33)
+            color=34;
         printf("\n");
-        for(int j=1; j<10; j++)
+        for(int j=1; j<11; j++)
         {
-             printf("%d ",i*j);
+            if(i==j)
+                printf("\x1b[%dm%3d\x1b[0m",33,i*j);
+            else
+                printf("\x1b[%dm%3d\x1b[0m",color,i*j);
         }
+        color++;
     }
     printf("\n");
 }
